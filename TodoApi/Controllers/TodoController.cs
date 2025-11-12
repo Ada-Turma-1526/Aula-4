dd519c00803a50ac170c0725c331e356f7938fa7
Paulo Ricardo Stradioti <paulo@paulo.eti.br>
2025-11-11 21:40:00 -0300

    Aula 3

---
+++ TodoApi/Controllers/TodoController.cs
@@ -0,0 +1,71 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers
+{
+    public class TodoController : ControllerBase
+    {
+        [Route("/api/todo")] // GET /api/todo
+        public IActionResult GetAllTodos()
+        {
+            return Ok(TodoList.Tasks.Values);
+        }
+
+        [Route("/api/todo/{id}")] // GET /api/todo/4
+        public IActionResult GetTodoById(int id)
+        {
+            if (!TodoList.Tasks.ContainsKey(id))
+            {
+                return NotFound();
+            }
+
+            return Ok(TodoList.Tasks[id]);
+        }
+
+        [HttpPost]
+        [Route("/api/todo")] // POST /api/todo
+        // Model Binding
+        public IActionResult CreateTodo([FromBody] Todo todo)
+        {
+            if (TodoList.Tasks.ContainsKey(todo.Id))
+                return Conflict(todo);
+
+            TodoList.Tasks.Add(todo.Id, todo);
+
+            return Created(); //asp.net vai mudar para um No Content.
+        }
+
+        [HttpPut]
+        [Route("/api/todo/{id}")] // POST /api/todo
+        // Model Binding
+        public IActionResult CreateTodo([FromBody] Todo todo, int id)
+        {
+            if (id != todo.Id)
+                return BadRequest();
+
+            if (!TodoList.Tasks.ContainsKey(id))
+                return NotFound();
+
+            var existing = TodoList.Tasks[id];
+
+            existing.Title = todo.Title;
+            existing.Description = todo.Description;
+            existing.IsCompleted = todo.IsCompleted;
+
+            return Ok(existing);
+        }
+
+        [HttpDelete]
+        [Route("/api/todo/{id}")]
+        public IActionResult Delete(int id)
+        {
+            if (!TodoList.Tasks.ContainsKey(id))
+                return NotFound();
+
+            TodoList.Tasks.Remove(id);
+
+            return NoContent();
+        }
+    }
+}
---
+++ TodoApi/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TodoApi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Criar um Builder de uma Web Application (Web Application é complexa -> Então usados o padrão
+            //  Builder)
+            var builder = WebApplication.CreateBuilder();
+
+            // Configurar serviços (Injeção de Dependência)
+            builder.Services.AddControllers();
+
+            // Constroi a aplicação (builder.Build())
+            var app = builder.Build();
+
+            app.MapControllers();
+
+            // Roda aplicação
+            app.Run();
+        }
+    }
+}
